1ab6dde8928b38c52d45f696759ca8a40a29f303
rublin <clInS1L978je>
2021-01-30 12:07:57 +0200

    create book controller and add swagger

--- src/main/java/com/holyrandom/library/LibraryApplication.java
+++ src/main/java/com/holyrandom/library/LibraryApplication.java
@@ -2,7 +2,9 @@ package com.holyrandom.library;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.oas.annotations.EnableOpenApi;
 
+@EnableOpenApi
 @SpringBootApplication
 public class LibraryApplication {
 
