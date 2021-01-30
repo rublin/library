8f8b3ef983c982033bf85255adc349379167611e
rublin <clInS1L978je>
2021-01-30 11:47:27 +0200

    init project

---
+++ src/test/java/com/holyrandom/library/LibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.holyrandom.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
